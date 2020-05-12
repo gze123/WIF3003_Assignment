ff5c7beb803ebc202e49e93c93a6f0dd66681a4b
Ziy En <gze.2315@gmail.com>
2020-05-12 22:08:13 +0800

    allow accept arguments from user

--- src/com/WIF3003_Assignment/Main.java
+++ src/com/WIF3003_Assignment/Main.java
@@ -1,8 +1,20 @@
 package com.WIF3003_Assignment;
 
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number of points you want to create (n): ");
+        int n = scanner.nextInt();
+        System.out.print("Enter number of thread you want to launch (value must be lesser or equal to n) (t): ");
+        int t = scanner.nextInt();
+        System.out.print("Enter the time you want the program to run (in second) (m): ");
+        int m = scanner.nextInt();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(t);
     }
 }
